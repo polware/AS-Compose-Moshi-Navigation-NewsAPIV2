39581d30fe174c58d8b0db63ba400068bd705776
Paul Vargas <76798994+polware@users.noreply.github.com>
2023-01-27 23:22:55 -0500

    Delete

--- NewsAPI2Compose/app/src/main/java/com/polware/newsapi2compose/models/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.polware.newsapi2compose.models
-
-data class Source(
-    val id: String? = null,
-    val name: String? = null
-)
--- NewsAPI2Compose/app/src/main/java/com/polware/newsapi2compose/models/TopNewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.polware.newsapi2compose.models
-
-data class TopNewsResponse(
-    val status: String? = null,
-    val totalResults: Int? = null,
-    val articles: List<Article>? = null
-)
